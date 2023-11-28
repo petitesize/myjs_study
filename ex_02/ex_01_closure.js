ff89ffe66c5325d0ca76d57ded2999904f8e48a6
jeeyng <petitesize22@gmail.com>
2023-11-28 16:01:25 +0900

    study: prototype

--- ex_02/ex_01_closure.js
+++ ex_02/ex_01_closure.js
@@ -1,3 +1,4 @@
+```클로저 함수```
 var arr = [];
 for (var i = 0; i < 5; i++) {
   arr[i] = (function (id) {
@@ -8,4 +9,18 @@ for (var i = 0; i < 5; i++) {
 }
 for (var index in arr) {
   console.log(arr[index]());
+}
+
+```아래의 실행 값은 5 5 5 5 5 이다.```
+
+var arr = []
+// for문이 실행되면 각 arr[i]는 retrun i를 가진다.
+// for문을 모두 마치면, i는 5가 되고, 각 arr[i]들은 return 5를 하기 때문에
+for(var i = 0; i < 5; i++){
+    arr[i] = function(){
+        return i;
+    }
+}
+for(var index in arr) {
+    console.log(arr[index]());
 }
---
+++ ex_03/prototype.js
@@ -0,0 +1,15 @@
+function Person(name) {
+  this.name = name;
+}
+Person.prototype.name = null;
+Person.prototype.introduce = function () {
+  return "My name is " + this.name;
+};
+
+function Programmer(name) {
+  this.name = name;
+}
+Programmer.prototype = new Person();
+
+var p1 = new Programmer("egoing");
+console.log(p1.introduce());
